cb80fe942cbe61657de9139c70f98996fd4b0ee8
Matt Teichman <teichman@uchicago.edu>
2026-08-01 15:49:39 -0500

    progress

--- lib/message.ml
+++ lib/message.ml
@@ -1,8 +1,10 @@
 let x = 5
 
 let blessed_keys =
-  [ "PkgName"; "InstalledVersion"; "FixedVersion" ]
+  [ "PkgName"; "InstalledVersion"; "FixedVersion"; "References" ]
 
+(* todo: figure out how to deal with References array to pull the CVE
+   URL out *)
 let string_to_string =
   let split s = Prelude.String.split ~sep:", " s in
   function
@@ -27,7 +29,17 @@ let safe_head = function
 let version_compare = Prelude.(on compare version_of_string)
 let ( ->- ) x y = version_compare x y = 1
 
-(* let rearrange_names lists = *)
+let is_cve_url url =
+  let regexp =
+    Str.regexp "^https://www.cve.org/CVERecord.*"
+  in
+  Str.string_match regexp url 0
+
+let get_url urls =
+  match List.filter is_cve_url urls with
+  | url :: _ -> Some url
+  | _ -> None
+
 let each_alist alist =
   let open Etude.Option in
   let+ name = List.assoc_opt "PkgName" alist >>= safe_head
@@ -74,33 +86,13 @@ let get_version current versionses =
   get_max nexts
 
 let lists_to_report lists =
+  let open Etude.Option in
   let each_pair ((name, current), versions) =
-    name, current, get_version current versions
+    let+ version = get_version current versions
+    in name, current, version
   in
-  coalesce_lists lists
-  |> Option.get
-  |> List.map each_pair
-
-(* # coalesce_lists lists |> Option.get |> List.map (fun ((name, current), versions) -> (name, current, get_version current versions));;
- * - : (string * string * string Etude.Option.t) list =
- * [("yaml", "1.10.2", Etude.Option.Some "1.10.3");
- *  ("wagtail", "7.0.6", Etude.Option.Some "7.0.7");
- *  ("urllib3", "2.6.3", Etude.Option.Some "2.7.0");
- *  ("serialize-javascript", "7.0.4", Etude.Option.Some "7.0.5");
- *  ("picomatch", "2.3.1", Etude.Option.Some "2.3.2");
- *  ("lodash", "4.17.23", Etude.Option.Some "4.18.0");
- *  ("js-yaml", "4.1.1", Etude.Option.Some "4.3.0");
- *  ("flatted", "3.3.3", Etude.Option.Some "3.4.2");
- *  ("brace-expansion", "1.1.12", Etude.Option.Some "1.1.17");
- *  ("bleach", "3.3.0", Etude.Option.Some "6.4.0");
- *  ("Pygments", "2.15.0", Etude.Option.Some "2.20.0");
- *  ("Django", "5.2.12", Etude.Option.Some "5.2.15");
- *  ("@babel/core", "7.29.0", Etude.Option.Some "7.29.6")] *)
-
-(* let coalesce_lists lists =
- *   let open Etude.Option in
- *   let+ traversed = traverse each_alist in
- *   coalesce traversed *)
+  let* coalesced = coalesce_lists lists in
+  traverse each_pair coalesced
 
 (* jq command *)
 (* trivy fs --quiet --scanners vuln --format json library_website | jq '.Results[]?.Vulnerabilities | select (. != null)[] | {"PkgName" : .PkgName, "InstalledVersion" : .InstalledVersion, "FixedVersion": .FixedVersion, "Severity" : .Severity }' | jq -n '[inputs]' > cvebump/example.json *)
