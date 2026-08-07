7e06dd9fe3dfcf419b587b02cc421578e76300b9
Matt Teichman <teichman@uchicago.edu>
2026-08-07 16:02:53 -0700

    still messing around with new logic that sidesteps jq

--- lib/process.ml
+++ lib/process.ml
@@ -1,7 +1,7 @@
 let x = 5
 
 let blessed_keys =
-  [ "PkgName"; "InstalledVersion"; "FixedVersion" ]
+  [ "PkgName"; "InstalledVersion"; "FixedVersion"; "References" ]
 
 let get_url urls =
   let is_cve_url url =
@@ -138,11 +138,15 @@ let trivy_output_to_vulns json =
   let open Etude.Option in
   let* results = find_path [ O "Results" ] json >>= unarray in
   let each_result json = find_path [ O "Vulnerabilities" ] json in
-  traverse each_result results
-  >>= traverse unarray
-  >>= traverse (traverse unobject)
-  >>| List.flatten
+  let+ vulns = traverse each_result results
+               >>= traverse unarray
+               >>= traverse (traverse unobject)
 
+
+let pull_names_out vulns =
+  let open Etude.Option in
+  let blessed (k, _) = List.mem k blessed_keys in
+  List.map (List.filter blessed) vulns
 
 (* jq command *)
 (* trivy fs --quiet --scanners vuln --format json library_website | jq '.Results[]?.Vulnerabilities | select (. != null)[] | {"PkgName" : .PkgName, "InstalledVersion" : .InstalledVersion, "FixedVersion": .FixedVersion, "Severity" : .Severity }' | jq -n '[inputs]' > cvebump/example.json *)
